f8f32b5f60e6951face3cd66d197f6d941f732c4
Ewinsforsont <sigge.ffgg@gmail.com>
2025-02-02 15:39:02 +0100

    Stored Item Tracking
    - Fixed bug where game would decreased the stored ammount of items when a diffrent item was removed from the spot
    - Is now tracking which item is stored by using Instance IDs

--- Assets/Scripts/S_StorageManager.cs
+++ Assets/Scripts/S_StorageManager.cs
@@ -10,26 +10,26 @@ public class S_StorageManager : MonoBehaviour
     #region Variables
     [SerializeField, Tooltip("This is a refrence to the Gamemanager that will keep track of the score")] private S_ScoreManager scoreManager;
     private S_ResourceManager resourceManager;
-    [SerializeField, Tooltip("If there is currently an item stored in the trigger box"), ReadOnly] private bool hasItem;
+    [Tooltip("What gameobject is currently stored")] private int storedItem = 0;
     #endregion
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger Entered");
-        if (other.gameObject.TryGetComponent<S_ResourceManager>(out resourceManager) && !hasItem)
+        if (other.gameObject.TryGetComponent<S_ResourceManager>(out resourceManager) && storedItem == 0)
         {
-            hasItem = true;
             scoreManager.ChangeScore(resourceManager.resource.type, resourceManager.resource.ammount);
+            storedItem = other.gameObject.GetInstanceID();
         }
         else
             Debug.Log("Failed to change resource");
     }
     private void OnTriggerExit(Collider other) 
     {
-        if (other.gameObject.TryGetComponent<S_ResourceManager>(out resourceManager) && hasItem)
+        if (other.gameObject.TryGetComponent<S_ResourceManager>(out resourceManager) && other.gameObject.GetInstanceID() == storedItem)
         {
             scoreManager.ChangeScore(resourceManager.resource.type, resourceManager.resource.ammount * -1);
-            hasItem = false;
+            storedItem = 0;
         }
     }
 }
